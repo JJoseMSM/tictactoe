e30fb8bb372db48fdc794ddab0acc74af6753d32
JoseMSM <nueljosema@gmail.com>
2024-05-21 12:00:18 -0600

    created the html and css portion of the project

---
+++ script.js
@@ -0,0 +1,16 @@
+const cell = document.querySelectorAll(".cell");
+const statusText = document.getElementById("texstatus");
+const restart = document.getElementById("restartbtn");
+
+const winConditions = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,5,6]
+];
+
+let optio
